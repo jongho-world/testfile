293debb8e97ae40020b7e0cb02f7bbb48fb6f184
jongho <151697658+jongho-world@users.noreply.github.com>
2023-11-22 13:15:16 +0900

    Add files via upload

---
+++ test.js
@@ -0,0 +1,67 @@
+const EM_IP="192.168.1.102";
+const R2_IP="192.168.1.108";
+const ITK_PORT=8443;
+
+// Resource
+const pd_resource="PickupDropoff"
+const d_resource="/Dropoff"
+
+// url: End Point 
+const EM_url="https://"+EM_IP+":"+ITK_PORT+d_resource;
+
+// JSON body (pickupdropoff)
+const pd_body={
+  "pickupGoal": "Goal1",
+  "pickupPriority": 10,
+  "dropoffGoal": "Goal2",
+  "dropoffPriority": 20
+};
+
+const dropoff_body={
+    "robot": "Sim108",
+    "goal": "Goal1",
+    "priority": 10,
+    "jobId": "test"
+}
+
+// 사용자 이름과 비밀번호
+const username = "toolkitadmin";
+const password = "O0K5WWqh2c6eCEntaLMac";
+
+// Basic Authentication 헤더 생성
+const basicAuthHeader = 'Basic ' + btoa(username + ':' + password);
+
+// 자체 서명된 인증서를 허용하도록 설정 (node.js 방식)
+//process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+console.log("Requesting URL:", EM_url);
+
+function Dropoff(){
+  fetch(EM_url,{
+    method: "POST",
+    //mode: 'no-cors',  // CORS 문제 해결을 위해 추가
+    // agent: new (require('https')).Agent({ rejectUnauthorized: false }),
+    headers:{
+      "accept": "application/json; charset=utf-8",
+      "Content-Type":"application/json; charset=utf-8",
+      "Authorization": basicAuthHeader, 
+    },
+    // JSON Body
+    body: JSON.stringify(dropoff_body),
+    })
+    // 응답 처리 1단계: Promise 함수 fulfilled, rejected 상태 판단
+    .then((response)=>{
+      console.log(response);
+      if(response.ok){
+        return response.json();
+      }
+      throw new Error("네트워크 통신 오류입니다.");
+    })
+    // 응답 처리 2단계: body 본문 취득 
+    .then((result)=>console.log(result))
+    .catch((error)=>console.log("error: ",error));
+}
+
+document.querySelector('div').addEventListener('click',Dropoff());
+
+s
